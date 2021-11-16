e3815262911b343335528e650c9f96670b68cc25
Abdul_rehman <76230556+Abdulrehman-PIAIC80387@users.noreply.github.com>
2021-11-16 17:35:46 +0500

    first push

---
+++ invoice_app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Invoice
+from .forms import InvoiceForm
+# Register your models here.
+
+class InvoiceAdmin(admin.ModelAdmin):
+   list_display = ['name', 'invoice_number', 'invoice_date']
+   form = InvoiceForm
+   list_filter = ['name']
+   search_fields = ['name']
+admin.site.register(Invoice, InvoiceAdmin)
---
+++ invoice_app/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render
+from .forms import InvoiceForm,InvoiceSearchForm,InvoiceUpdateForm
+from .models import *
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+# Create your views here.
+def home(request):
+	title = 'Welcome: This is the Home Page'
+	context = {
+	"title": title,
+	}
+	return render(request, "home.html",context)
+
+
+def add_invoice(request):
+	form = InvoiceForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		messages.success(request, 'Successfully Saved')
+		return redirect('/list_invoice')
+	context = {
+		"form": form,
+		"title": "New Invoice",
+	}
+	return render(request, "entry.html", context)
+
+		
+    
+
+
+
+def list_invoice(request):
+	title = 'List of Invoices'
+	queryset = Invoice.objects.all()
+	form = InvoiceSearchForm(request.POST or None)
+	
+	context = {
+		"title": title,
+		"queryset": queryset,
+		"form":form
+	}
+
+	if request.method == 'POST':
+		queryset = Invoice.objects.filter(invoice_number__icontains=form['invoice_number'].value(),
+										name__icontains=form['name'].value()
+										)
+		context = {
+		"form": form,
+		"title": title,
+		"queryset": queryset,
+	}
+	return render(request, "list_invoice.html", context)    
+	
+def update_invoice(request, pk):
+	queryset = Invoice.objects.get(id=pk)
+	form = InvoiceUpdateForm(instance=queryset)
+	if request.method == 'POST':
+		form = InvoiceUpdateForm(request.POST, instance=queryset)
+		if form.is_valid():
+			form.save()
+			return redirect('/list_invoice')
+
+	context = {
+		'form':form
+	}
+	return render(request, 'entry.html', context)
+
+
+def delete_invoice(request, pk):
+	queryset = Invoice.objects.get(id=pk)
+	if request.method == 'POST':
+		queryset.delete()
+		return redirect('/list_invoice')
+	return render(request, 'delete_invoice.html')
